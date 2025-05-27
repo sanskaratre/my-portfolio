0ac5fb2466b3e42292a2df6b0740b8cfb3cd3afa
sanskaratre <100466873+sanskaratre@users.noreply.github.com>
2025-05-27 18:04:22 +0530

    Add build to git

---
+++ client/build/precache-manifest.8d4ffeeb8d57300788164d0a6254c3fc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0f70da54c29e2d038eefc42af8237e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5945e33d808a5d397cfe",
+    "url": "/static/css/2.a5344151.chunk.css"
+  },
+  {
+    "revision": "2c0996df2a282c2f1c86",
+    "url": "/static/css/main.1bed7da1.chunk.css"
+  },
+  {
+    "revision": "5945e33d808a5d397cfe",
+    "url": "/static/js/2.7c99454e.chunk.js"
+  },
+  {
+    "revision": "075ee0fe56affc8020645735366f8cbd",
+    "url": "/static/js/2.7c99454e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9dd26d7a3f3b758130f",
+    "url": "/static/js/3.0508f991.chunk.js"
+  },
+  {
+    "revision": "2c0996df2a282c2f1c86",
+    "url": "/static/js/main.0a8a0b93.chunk.js"
+  },
+  {
+    "revision": "8ac2f384869e8bb25b74",
+    "url": "/static/js/runtime-main.34f61dbc.js"
+  },
+  {
+    "revision": "fa273a07348279ccad83b83e0655424f",
+    "url": "/static/media/E-comm.fa273a07.png"
+  },
+  {
+    "revision": "94c57921a8e402f34e6f8c23bcf5d75e",
+    "url": "/static/media/Fullstack.94c57921.pdf"
+  },
+  {
+    "revision": "1401d5bfbd02d3ea1d4254ed0ccd23fb",
+    "url": "/static/media/Profile_pic.1401d5bf.jpg"
+  }
+]);
